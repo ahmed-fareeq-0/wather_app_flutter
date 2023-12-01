3daf42eb858e6b88218c07e806f1bfadd0811154
ahmed-fareeq-0 <ahmed.fareeq.0@gmail.com>
2023-12-02 01:23:07 +0300

    update

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -8,166 +8,175 @@ import '../../widget/myChart.dart';
 class HomeScreen extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GetBuilder<HomeController>(builder: (controller) {
-        return SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(top: 60, left: 20, right: 20),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            controller.updateWeather();
-                          },
-                          child: Container(
-                            width: 49,
-                            height: 49,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10),
-                                bottomLeft: Radius.circular(10),
-                              ),
-                            ),
-                            child: Icon(
-                              Icons.search,
-                              color: Color(0xff31304D),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 5,
-                          child: TextField(
-                            textAlign: TextAlign.right,
-                            onChanged: (value) => controller.city = value,
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                            textInputAction: TextInputAction.search,
-                            onSubmitted: (value) => controller.updateWeather(),
-                            decoration: InputDecoration(
-                              hintStyle: TextStyle(color: Colors.white),
-                              hintText: 'البحث',
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 10),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.only(
-                                  bottomRight: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                                borderSide: BorderSide(color: Colors.white),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.only(
-                                  bottomRight: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                                borderSide: BorderSide(color: Colors.white),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.only(
-                                  bottomRight: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                                borderSide: BorderSide(color: Colors.white),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  controller.isLoading
-                      ? WeatherCard(controller: controller)
-                      : Container(
-                          height: 280,
-                          child: Center(
-                            child: CircularProgressIndicator(),
-                          ),
-                        )
-                ],
-              ),
-              Column(
-                children: [
-                  Stack(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15),
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      return Scaffold(
+        body: GetBuilder<HomeController>(builder: (controller) {
+          return SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(
+                      top: constraints.maxHeight * 0.3 / 4,
+                      left: 20,
+                      right: 20),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          controller.updateWeather();
+                        },
                         child: Container(
-                          padding: EdgeInsets.only(top: 16),
-                          child: Align(
-                            alignment: Alignment.topLeft,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.only(left: 5, right: 5),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.arrow_circle_right,
-                                        color: Colors.white70,
-                                      ),
-                                      Text(
-                                        'توقعات الأيام القادمة',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .caption!
-                                            .copyWith(
-                                              fontSize: 16,
-                                              fontFamily: 'flutterfonts',
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.white70,
-                                            ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 8,
-                                ),
-                                MyChart(),
-                                SizedBox(
-                                  height: 15,
-                                ),
-                                Container(
-                                  padding: EdgeInsets.only(right: 5),
-                                  child: Text(
-                                    'مدن اخرى'.toUpperCase(),
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .caption!
-                                        .copyWith(
-                                          fontSize: 16,
-                                          fontFamily: 'flutterfonts',
-                                          color: Colors.white70,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 8,
-                                ),
-                                MyList(),
-                              ],
+                          width: 49,
+                          height: 49,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10),
+                            ),
+                          ),
+                          child: Icon(
+                            Icons.search,
+                            color: Color(0xff31304D),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 5,
+                        child: TextField(
+                          textAlign: TextAlign.right,
+                          onChanged: (value) => controller.city = value,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                          textInputAction: TextInputAction.search,
+                          onSubmitted: (value) => controller.updateWeather(),
+                          decoration: InputDecoration(
+                            hintStyle: TextStyle(color: Colors.white),
+                            hintText: 'البحث',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(10),
+                                topRight: Radius.circular(10),
+                              ),
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(10),
+                                topRight: Radius.circular(10),
+                              ),
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(10),
+                                topRight: Radius.circular(10),
+                              ),
+                              borderSide: BorderSide(color: Colors.white),
                             ),
                           ),
                         ),
                       ),
                     ],
                   ),
-                ],
-              ),
-            ],
-          ),
-        );
-      }),
-    );
+                ),
+                Column(
+                  children: [
+                    controller.isLoading
+                        ? WeatherCard(controller: controller)
+                        : Container(
+                            height: constraints.maxHeight * 0.25,
+                            child: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Stack(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          child: Container(
+                            padding: EdgeInsets.only(top: 16),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.only(left: 5, right: 5),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.arrow_circle_right,
+                                          color: Colors.white70,
+                                        ),
+                                        Text(
+                                          'توقعات الأيام القادمة',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .caption!
+                                              .copyWith(
+                                                fontSize: 16,
+                                                fontFamily: 'flutterfonts',
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white70,
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: constraints.maxHeight * 0.02,
+                                  ),
+                                  MyChart(),
+                                  SizedBox(
+                                    height: constraints.maxHeight * 0.03,
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.only(right: 5),
+                                    child: Text(
+                                      'مدن اخرى'.toUpperCase(),
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .caption!
+                                          .copyWith(
+                                            fontSize: 16,
+                                            fontFamily: 'flutterfonts',
+                                            color: Colors.white70,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: constraints.maxHeight * 0.02,
+                                  ),
+                                  MyList(),
+                                  SizedBox(
+                                    height: constraints.maxHeight * 0.02,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }),
+      );
+    });
   }
 }
